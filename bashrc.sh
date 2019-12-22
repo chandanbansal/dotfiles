f66d9448b945422587f65865f945a3e12bef40c6
Chandan Bansal <chandan.bansal@flipkart.com>
2019-12-22 20:31:13 +0530

    added new structure reference: https://github.com/naggie/dotfiles

--- bashrc.sh
+++ bashrc.sh
@@ -1,24 +1,8 @@
-#HOME Needs /usr/local/bin before /usr/bin
-homebrew=/usr/local/bin:/usr/local/sbin
-export PATH=$homebrew:$PATH
+source ~/.env.sh
+source ~/.functions.sh
+source ~/.aliases
 
-#for java 1.6
-#Download from https://support.apple.com/kb/DL1572?locale=en_US
-#export JAVA_HOME=$(/usr/libexec/java_home -v 1.6)
-
-#for java 1.7
-#export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
-
-#for java 1.8
-export JAVA_HOME=$(/usr/libexec/java_home)
-
-
-export PATH=.:/Users/chandan.bansal/Dropbox/bash/bin:/usr/local/mysql/bin:$HOME/local/node/bin:/private/var/www/node_modules/node.io/bin:$PATH:$JAVA_HOME/bin:/usr/local/go/bin
-#export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
-
-export PATH=$DOTFILES/bin:$PATH
-source $DOTFILES/bin/quotescow
-
+#source $DOTFILES/bin/quotescow
 ############### Improve bash history ####################
 
 #history format, This will add timestamp to bash_history file. But when we run "history" command in terminal, it will show formated time.
@@ -30,7 +14,7 @@ export HISTTIMEFORMAT="%Y.%m.%d %H:%M:%S> "
 #ignore space will ignore commands starting with space
 #ignoredups only ignore continous duplicates. 
 #erasedups causes all previous lines matching the current line to be removed from the history list before that line is saved. But its not working somehow
-HISTCONTROL=ignoredups:erasedups
+HISTCONTROL=ignoredups:ignorespace:erasedups
 
 HISTIGNORE="free*:pwd*:df -h*"
 
@@ -40,57 +24,45 @@ HISTSIZE=100000
 
 # append history entries..
 # created a .bash_logout in home with "history -a \n history -w". That works great
+
+# update the values of LINES and COLUMNS. Automatically
+shopt -s checkwinsize
+
 shopt -s histappend
 
+HISTFILESIZE=$HISTSIZE
 # After each command, save and reload history and delete duplicates. But doesn't work with HISTTIMEFORMAT
 # from http://unix.stackexchange.com/questions/18212/bash-history-ignoredups-and-erasedups-setting-conflict-with-common-history/18443#18443?newreg=092a797bc79b47629a88552d4e44d3e3
 #PROMPT_COMMAND="history -n; history -w; history -c; history -r; $PROMPT_COMMAND"
 ############### Improve bash history ends ####################
 
-#modify mvn output
-export MVN=/usr/local/bin/mvn
+# if you call a different shell, this does not happen automatically. WTF?
+export SHELL=$(which bash)
 
-#colorize the maven output
-function mvn {
-    $MVN $@ | colorize.pl -f mvn
-}
+#PS1="
+#\$(__exit_warn)
+#\[\e[36m\]\${CMD_TIMER_PROMPT}\[\e[38;5;${PROMPT_COLOUR}m\]\u@\H:\$PWD\[\e[38;5;243m\]\$(__git_prompt)\$(__p4_prompt)\[\e[0m\]
+#\$ "
 
-function duf {
-    sudo du -shk "$@" | sort -n | while read size fname; do for unit in k M G T P E Z Y; do if [ $size -lt 1024 ]; then echo -e "${size}${unit}\t${fname}"; break; fi; size=$((size/1024)); done; done
-}
+# fix backspace on some terminals
+stty erase ^?
 
-#colored svn diff
-function svndiff () {
-  svn diff -x -b ${1+"$@"} | colordiff | less -r
-}
+_disable_flow_control
 
-#git autocomplete
-if [ -f $DOTFILES/data/git-completion.bash ]; then
-  . $DOTFILES/data/git-completion.bash
+
+# Source bash completions
+# see https://discourse.brew.sh/t/bash-completion-2-vs-brews-auto-installed-bash-completions/2391/4
+# for discussion. Mac requires brew packages bash and bash-completion@2
+if [[ -e "/usr/local/share/bash-completion/bash_completion" ]]; then
+    # mac os, new (jit) and few remaining old style completions (git!)
+	export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
+	source "/usr/local/share/bash-completion/bash_completion"
+elif [[ -e "/usr/local/etc/profile.d/bash_completion.sh" ]]; then
+    # mac os, new style completions only
+	source "/usr/local/etc/profile.d/bash_completion.sh"
+elif [[ -e "/etc/bash_completion" ]]; then
+    # standard on linux systems, sources from /etc/bash_completion.d/
+	source "/etc/bash_completion"
 fi
-
-
-#ssh with save password
-fssh () {  if ! ssh -o 'PasswordAuthentication=no' "$@"; then ssh-copy-id "$@"; ssh "$@"; fi  }
-
-# auto colorify the grep output
-alias grep="grep --color=auto"
-
-export CLICOLOR=1
-export LSCOLORS=GxFxCxDxBxegedabagaced
-
-CDPATH=.:/var/www:$CDPATH
-
-#kill all the grep matching processes
-killgrep() {
-    sudo kill -9 `ps -aef | grep $@ | awk '{print $2}'`
-}
-
-function stt() {
-    ## set terminal title
-    echo -en "\033]2;$1\007"
-    #PS1=$(echo $PS1 |sed 's/^.*\\007//')
-    #export PS1="\033]0;$1\007$PS1"
-}
 
 
