ea5ceddd291e8d77ff0ca676ff2a74bfd4e7416e
Chandan Bansal <chandanbansal2004@gmail.com>
2023-01-08 00:04:15 +0530

    removing unnecessary packages

--- system-dependencies/MACOS_AMD64.sh
+++ system-dependencies/MACOS_AMD64.sh
@@ -14,23 +14,14 @@ brew unlink md5sha1sum || true
 
 # Upgrade or install (logic necessary)
 packages=(
-    bash    
     coreutils
-    csshx
     ffmpeg
     git
-    gnupg2
     htop
     jq
     maven
-    mpssh
-    openssl
-    openssh
     pass
     perl
-    python
-    ssh-copy-id
-    tmux
     tree
     vim
     wget
@@ -39,17 +30,3 @@ packages=(
 for package in "${packages[@]}"; do
     brew upgrade $package || brew install $package
 done
-
-# create alias for gsha256sum
-#ln -sf /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
-
-# browserpass host
-#TARGZ="$(
-#    obtain \
-#        https://github.com/browserpass/browserpass-native/releases/download/3.0.6/browserpass-darwin64-3.0.6.tar.gz \
-#        422bc6dd1270a877af6ac7801a75b4c4b57171d675c071470f31bc24196701e3
-#)"
-#[ -f /usr/local/bin/browserpass ] && sudo rm -f /usr/local/bin/browserpass
-#sudo tar -C /usr/local/bin/ --strip=1 -xzf "$TARGZ" browserpass-darwin64-3.0.6/browserpass-darwin64
-#sudo mv /usr/local/bin/browserpass-darwin64 /usr/local/bin/browserpass
-
